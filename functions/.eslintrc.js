cf8d13fc958265a424d199a75a1a3a6009bcacff
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2026-03-09 14:21:08 -0400

    fix eslint

---
+++ functions/.eslintrc.js
@@ -0,0 +1,38 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    // "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json"],
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+    "/.eslintrc.js",
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+    "import/no-unresolved": 0,
+    "indent": ["error", 2],
+    "object-curly-spacing": ["error", "always"],
+    "max-len": ["error", { "code": 120 }],
+    "linebreak-style": 0, // Disable linebreak-style to avoid CRLF issues on Windows.
+  },
+};
