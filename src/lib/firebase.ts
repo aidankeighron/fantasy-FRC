ba4cf529ea4a5bff99a3eab5f55c6153400800dd
aidankeighron <aidankeighron@gmail.com>
2026-03-18 11:53:15 -0400

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,7 @@
 
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFunctions } from "firebase/functions";
 
@@ -34,6 +34,7 @@ const firebaseConfig = getFirebaseConfig();
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence).catch(console.error);
 const storage = getStorage(app);
 const functions = getFunctions(app);
 
